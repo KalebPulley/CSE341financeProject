5b2a70b830df3a440ad424170c70061c1f473f47
KalebPulley <pul21001@byui.edu>
2022-09-28 20:43:51 -0600

    api-docs renamed

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,8 +2,8 @@ const routes = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
-routes.use("/docs", swaggerUi.serve);
-routes.get("/docs", swaggerUi.setup(swaggerDocument));
+routes.use("/api-docs", swaggerUi.serve);
+routes.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 //export the module
 module.exports = routes;
