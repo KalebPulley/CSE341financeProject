2ac3f728ba32f5ca7010b5ddaed0d8fa0436d37d
KalebPulley <pul21001@byui.edu>
2022-10-01 23:28:57 -0600

    api see if it loads over night

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require("swagger-autogen")();
 const doc = {
   info: {
     title: "My API",
-    description: "Contacts API",
+    description: "finance API",
   },
   host: "localhost:3000",
   schemes: ["http"],
