e91abc37346c60fe7d72f9edb0cf511d171d6370
KalebPulley <pul21001@byui.edu>
2022-10-08 23:27:19 -0600

    oauth begining

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const mongodb = require("./database/");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const { auth, requiresAuth } = require('express-openid-connect');
 //make envirment variable available
 dotenv.config();
 
@@ -18,6 +19,16 @@ process.on("uncaughtException", (err, origin) => {
   );
 });
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+
 //initalize database and start the server
 mongodb.initializeDatabase((err) => {
   if (err) {
@@ -28,3 +39,19 @@ mongodb.initializeDatabase((err) => {
     });
   }
 });
+
+// app.use(auth(config));
+
+// app.get("/login", (req, res) => {
+//   res.redirect(
+//     `https://github.com/login/oauth/authorize?client_id=${process.env.clientID}`
+//   )
+// } )
+
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
+
+// app.get('/profile', requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require("express").Router();
+const { requiresAuth } = require('express-openid-connect');
 routes.use("/", require("./swagger"));
-routes.use("/users", require("./users"));
+routes.use("/users", requiresAuth(), require("./users"));
 routes.use("/transactions", require("./transactions"));
 //export the module
 module.exports = routes;
